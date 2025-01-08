b0477ede20e2784cbc59dc9088e9229bd028e21b
navneetthakur07 <navneetthakurz47@gmail.com>
2025-01-08 21:37:13 +0530

    third commit

--- src/main/java/com/example/MyApp/model/HelloWorldEntity.java
+++ src/main/java/com/example/MyApp/model/HelloWorldEntity.java
@@ -4,5 +4,28 @@ public class HelloWorldEntity {
     private int number;
     private String name;
     private boolean bool;
-    
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isBool() {
+        return bool;
+    }
+
+    public void setBool(boolean bool) {
+        this.bool = bool;
+    }
 }
---
+++ src/main/java/com/example/MyApp/model/HelloWorldPart2.java
@@ -0,0 +1,31 @@
+package com.example.MyApp.model;
+
+public class HelloWorldPart2 {
+    private int number;
+    private String name;
+    private Long mobileNumber;
+
+    public int getNumber(){
+        return number;
+    }
+
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name= name;
+    }
+
+    public Long getMobileNumber(){
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(Long mobileNumber){
+        this.mobileNumber=mobileNumber;
+    }
+}
